4ba7ac5da3c19c9cb3261206f886dbc975350277
Danny Lin <danny@kdrag0n.dev>
2021-01-13 19:08:28 -0800

    Fix mismatching vendor and system security patch levels

---
+++ post-fs-data.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+set -eufo pipefail
+
+system_patch="$(getprop ro.build.version.security_patch)"
+vendor_patch="$(getprop ro.vendor.build.security_patch)"
+
+# Fix mismatching security patch levels
+if [[ "$system_patch" != "$vendor_patch" ]]; then
+    resetprop ro.build.version.security_patch "$vendor_patch"
+    resetprop ro.build.version.real_security_patch "$system_patch"
+fi
