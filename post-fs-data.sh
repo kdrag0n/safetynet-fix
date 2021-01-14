7df5188942809cc363a76f15de02ad29db3b6d20
Danny Lin <danny@kdrag0n.dev>
2021-01-14 00:05:36 -0800

    post-fs-data: Ignore empty vendor security patch levels

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,8 +5,8 @@ set -eufo pipefail
 system_patch="$(getprop ro.build.version.security_patch)"
 vendor_patch="$(getprop ro.vendor.build.security_patch)"
 
-# Fix mismatching security patch levels
-if [[ "$system_patch" != "$vendor_patch" ]]; then
+# Fix mismatching security patch levels if vendor patch is not empty
+if [[ ! -z "$vendor_patch" ]] && [[ "$system_patch" != "$vendor_patch" ]]; then
     resetprop ro.build.version.security_patch "$vendor_patch"
     resetprop ro.build.version.real_security_patch "$system_patch"
 fi
