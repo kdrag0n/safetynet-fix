0d788c1702f9c895c626c4a28a52692e59283caf
Danny Lin <danny@kdrag0n.dev>
2021-09-06 01:24:21 -0700

    Remove old module files

--- service.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Keystore v2 starts before Magisk on Android 12, so it needs to be restarted.
-# Do this in service.sh to make sure that files have been mounted already.
-sdk="$(getprop ro.build.version.sdk)"
-if [[ "$sdk" -ge 31 ]]; then
-    stop keystore2
-    start keystore2
-fi
