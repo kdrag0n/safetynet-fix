2f56b7320cdacf97758d2283e75454505f6e3737
Danny Lin <danny@kdrag0n.dev>
2021-09-06 03:43:04 -0700

    java: Append space character to device model name
    This bypasses Google's new device-based hardware attestation enforcement
    as of September 2, 2021.

---
+++ java_module/app/src/main/java/dev/kdrag0n/safetynetriru/BuildHooks.kt
@@ -0,0 +1,13 @@
+package dev.kdrag0n.safetynetriru
+
+import android.os.Build
+
+internal object BuildHooks {
+    fun init() {
+        // Append a space to the device model name
+        Build::class.java.getDeclaredField("MODEL").let { field ->
+            field.isAccessible = true
+            field.set(null, Build.MODEL + " ")
+        }
+    }
+}
--- java_module/app/src/main/java/dev/kdrag0n/safetynetriru/EntryPoint.kt
+++ java_module/app/src/main/java/dev/kdrag0n/safetynetriru/EntryPoint.kt
@@ -5,8 +5,9 @@ object EntryPoint {
     @JvmStatic
     fun init() {
         runCatching {
-            logDebug("Entry point: Initializing SafetyNet patch")
+            logDebug("Entry point: Initializing SafetyNet patches")
             SecurityBridge.init()
+            BuildHooks.init()
         }.recoverCatching { e ->
             // Throwing an exception would require the JNI code to handle exceptions, so just catch
             // everything here.
