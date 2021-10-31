8015d3678642cac9d08c0121374e3a843b8f2001
Danny Lin <danny@kdrag0n.dev>
2021-10-30 17:50:24 -0700

    Remove Riru implementation
    Zygisk is now the primary implementation of this module.
    
    NB: The Zygisk module template is in the public domain, so attribution
    is no longer needed in the license.

--- riru/module/src/main/cpp/include/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace riru {
-    extern const int moduleVersionCode;
-    extern const char* const moduleVersionName;
-    extern const int moduleApiVersion;
-    extern const int moduleMinApiVersion;
-}
--- riru/module/src/main/cpp/template/config.cpp
+++
@@ -1,8 +0,0 @@
-#include "config.h"
-
-namespace riru {
-    const int moduleVersionCode = ${RIRU_MODULE_VERSION};
-    const char* const moduleVersionName = "${RIRU_MODULE_VERSION_NAME}";
-    const int moduleApiVersion = ${RIRU_MODULE_API_VERSION};
-    const int moduleMinApiVersion = ${RIRU_MODULE_MIN_API_VERSION};
-}
