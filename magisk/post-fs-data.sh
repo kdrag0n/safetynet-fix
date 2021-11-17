7e4652977a9060fd8fbc619e3605ca003d9b1c8d
osm0sis <osm0sis@outlook.com>
2021-11-17 15:56:20 -0400

    magisk: Ensure DenyList is enabled
    - DenyList must be enabled to pass basicIntegrity
    - also allows seamlessly upgrading to Magisk 23010+ and USNF 2.2.x since Magisk currently only allows Zygisk setting to stay enabled via the Magisk app before the update reboot with DenyList showing enabled but reverts post-reboot

--- magisk/post-fs-data.sh
+++ magisk/post-fs-data.sh
@@ -1,4 +1,7 @@
 #!/system/bin/sh
 
+# DenyList must be enabled to pass basicIntegrity
+magisk --denylist enable
+
 # Remove Play Services from DenyList, otherwise the Zygisk module won't load
 magisk --denylist rm com.google.android.gms
