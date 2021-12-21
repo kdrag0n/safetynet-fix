8ea2649fa0310f40b0831e9ec48c325536de3c14
Danny Lin <danny@kdrag0n.dev>
2021-12-21 15:40:57 -0800

    customize: Prevent installation on Android 7.x and older
    Android 7.x lacks the InMemoryDexClassLoader API, which is necessary for
    the module to load Java code, and is unlikely to support hardware
    attestation on any production devices anyway.
    
    Fixes #124, #127

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,3 +1,12 @@
 #!/system/bin/sh
 
+# Android 8.0 or newer
+if [[ "$(getprop ro.build.version.sdk)" -lt 26 ]]; then
+    ui_print "This module only supports Android 8.0 and newer."
+
+    # Abort install and clean up
+    rm -fr "$TMPDIR" "$MODPATH"
+    exit 1
+fi
+
 chmod 755 "$MODPATH/service.sh" "$MODPATH/post-fs-data.sh"
