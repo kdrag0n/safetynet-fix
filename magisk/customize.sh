b3893d32f37d80a455c1873c6d63738ec63e68a1
Danny Lin <danny@kdrag0n.dev>
2022-03-12 20:40:53 -0800

    magisk: Allow limited installation on Android 7 and older
    Key attestation was introduced in Android 7.0, but Android 7.x doesn't
    have InMemoryDexClassLoader so our Zygisk module is incompatible.
    
    In general, users on such old versions of Android don't need to bypass
    hardware-backed attestation (which isn't even applicable on Android 6
    and older), so allow them to install the module without the Zygisk part.
    
    Closes #156.

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -2,11 +2,13 @@
 
 # Android 8.0 or newer
 if [[ "$(getprop ro.build.version.sdk)" -lt 26 ]]; then
-    ui_print "This module only supports Android 8.0 and newer."
+    ui_print ""
+    ui_print "Functionality is limited on Android 7 and older."
+    ui_print "Hardware-backed attestation will not be disabled."
+    ui_print ""
 
-    # Abort install and clean up
-    rm -fr "$TMPDIR" "$MODPATH"
-    exit 1
+    # Remove Zygisk module, but keep props and scripts
+    rm -fr "$MODPATH/zygisk"
 fi
 
 chmod 755 "$MODPATH/service.sh" "$MODPATH/post-fs-data.sh"
